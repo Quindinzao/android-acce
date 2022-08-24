dab82e48dc97acf2414943fe2764443c495d80ae
Joao Fernandes <joao.fernandes@impar.com.br>
2022-08-24 02:55:38 -0300

    first commit

---
+++ app/src/main/java/com/quindinzao/acce/SplashScreenActivity.java
@@ -0,0 +1,28 @@
+package com.quindinzao.acce;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        newScreen();
+    }
+
+    private void newScreen() {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent changeScreen = new Intent(SplashScreenActivity.this, MainActivity.class);
+                startActivity(changeScreen);
+                finish();
+            }
+        }, 3000);
+    }
+}
